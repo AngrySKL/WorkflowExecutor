a384d8ae044471df55ad56594f186c900f456f8a
Veal Wang <49362109@qq.com>
2023-08-20 21:31:37 +0800

    Update IHandlerWrapper.cs

--- src/WorkflowExecutor.Core/Common/IHandlerWrapper.cs
+++ src/WorkflowExecutor.Core/Common/IHandlerWrapper.cs
@@ -3,6 +3,6 @@ using MediatR;
 
 namespace WorkflowExecutor.Core.Common;
 
-public interface IHandlerWrapper<in TRequest, TResponse> :
-    IRequestHandler<TRequest, Result<TResponse>> where TRequest : IRequestWrapper<TResponse>
+public interface IHandlerWrapper<in TRequest, TResponse> : IRequestHandler<TRequest, Result<TResponse>>
+    where TRequest : IRequestWrapper<TResponse>
 { }
