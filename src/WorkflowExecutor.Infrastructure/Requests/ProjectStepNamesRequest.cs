44f72d095f15407354ed3086e440f46e6926db6b
Veal Wang <49362109@qq.com>
2023-09-23 10:45:25 +0800

    添加 request validator

--- src/WorkflowExecutor.Infrastructure/Requests/ProjectStepNamesRequest.cs
+++ src/WorkflowExecutor.Infrastructure/Requests/ProjectStepNamesRequest.cs
@@ -1,8 +1,23 @@
-﻿namespace WorkflowExecutor.Infrastructure.Requests;
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace WorkflowExecutor.Infrastructure.Requests;
 
 public record ProjectStepNamesRequest(string ProjectName)
 {
     public const string Route = "/Project/{ProjectName}/Steps";
 
     public static string BuildRoute(string projectName) => Route.Replace("{ProjectName}", projectName);
+}
+
+public class ProjectStepNamesRequestValidator : Validator<ProjectStepNamesRequest>
+{
+    public ProjectStepNamesRequestValidator()
+    {
+        RuleFor(request => request.ProjectName)
+            .NotEmpty()
+            .WithMessage("project name could not be empty")
+            .NotEqual("123")
+            .WithMessage("project name could not be 123");
+    }
 }
